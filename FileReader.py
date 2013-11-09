f1ed2103e935e84bb75963417ceb085a716b2270
Hussein Kaddoura <hussein.nawwaf@gmail.com>
2013-11-09 14:21:38 -0700

    refactoring adding the leader traits

--- FileReader.py
+++ FileReader.py
@@ -2,7 +2,7 @@
 
 import struct
 import zlib
-from bitstring import Bits
+from bitstring import Bits, ConstBitStream
 
 __author__ = "Hussein Kaddoura"
 __copyright__ = "Copyright 2013, Hussein Kaddoura"
@@ -19,158 +19,141 @@ class FileReader:
     def __init__(self, filename):
         self.filename = filename
         if isinstance(filename, str):
-            self.r = open(filename, 'rb')
+            self.file = open(filename, 'rb')
+            self.bits = ConstBitStream(open(filename, 'rb'))
 
     def __enter__(self):
         return self
 
     def __exit__(self, type, value, traceback):
-        self.r.close()
+        self.file.close()
 
-    def skip(self,count=1):
-        self.r.seek(count, 1)
+    def skip_bytes(self,count=1):
+        self.bits.read('pad:{0}'.format(count*8))
 
-    def read_byte(self):
-        """ Read a single byte as an integer value """
-        t = self.r.read(1)
-        if len(t) != 1:
-            self.eof = True
-            return 0
-        return ord(t)
+    def peek_int(self):
+        return self.bits.peek(32).uintle
+
+    def forward_to_first_non_zero_byte(self, start, end):
+        self.bits.pos = start
+        while self.bits.pos < end and self.bits.read(8).intle == 0:
+            pass
+        self.bits.pos -= 8
+
+    def read_strings_from_block(self, start, end, stopAtEmptyString=False):
+        self.bits.pos = start
+        r = []
+        while self.bits.pos < end:
+            s = self.read_string()
+            if s == "" and stopAtEmptyString:
+                return r
+            r.append(s)
+        return tuple(r)
 
     def read_int(self):
         """ Read a single little endian 4 byte integer """
-        # My *guess* is that they're all signed
-        t = self.r.read(4)
-        if len(t) != 4:
-            self.eof = True
-            return 0
-        return struct.unpack("<i", t)[0]
+        return self.bits.read(32).intle
 
-    def read_ints(self, count=None, esize=1):
-        """ Read count tuples of esize little endian 4 byte integers and return them in a list. If count is omitted, read it as a 4 byte integer first """
-        if count is None:
-            count = self.read_int()
-        temp = []
-        while count > 0:
-            if esize > 1:
-                t = []
-                for i in range(esize):
-                    t.append(self.read_int())
-                temp.append(tuple(t))
-            else:
-                temp.append(self.read_int())
-            count -= 1
-        return temp
+    def findall(self,bs):
+        return self.bits.findall(bs,bytealigned=True)
+
+    def read_bytes(self, count):
+        return self.bits.read(count*8)
+
+    def read_byte(self):
+        return self.bits.read(8).uint
+
+    def find(self, bs, start, end):
+        return self.bits.find(bs,start, end, True )
+
+    @property
+    def pos(self):
+        return self.bits.pos
+
+    @pos.setter
+    def pos(self, value):
+        self.bits.pos = value
 
     def read_string(self):
         """ Read an undelimited string with the length given in the first 4 bytes """
-        return self.r.read(self.read_int()).decode("utf-8", 'replace')
+        return self.bits.read('bytes:{0}'.format(self.read_int())).decode("utf-8", 'replace')
+    #
+    # def find(self, bytes, forward = True, offset = -4, searchFromCurrentPos = False):
+    #     """
+    #     Searches a byte object in the file.
+    #     forward = True : sets the position on the first occurrence of the pattern
+    #     returns position of the pattern of -1 if not found
+    #     """
+    #     curPos = self.r.tell()
+    #     if not searchFromCurrentPos:
+    #         self.r.seek(0)
+    #
+    #     f = Bits(self.r)
+    #     occ = f.find(bytes, bytealigned=True, start=int(self.r.tell()*8))
+    #
+    #     if len(occ)> 0:
+    #         pos = int(occ[0]/8)
+    #         if forward:
+    #             self.r.seek(pos + offset)
+    #         else:
+    #             self.r.seek(curPos)
+    #
+    #         return pos + offset
+    #
+    #     return 0 #not found
+    #
+    # def findall(self, bytes):
+    #     f = Bits(self.r)
+    #     return map(lambda x: int(x/8), f.findall(bytes))
+    #
+    #
+    # def forward_string(self, bytes):
+    #     return self.find(bytes, True)
+    #
+    #
+    # def extract_compressed_data(self):
+    #     curpos = self.r.tell()
+    #     # self.r.seek(0)          #go the start of the file
+    #     # data = self.r.read()    #read all the file's data
+    #     # find the start of the zlib magic number 78 9C EC BD
+    #
+    #     i=0
+    #     occ = self.findall('0x789C')
+    #     for pos in occ:
+    #         self.r.seek(pos)
+    #
+    #         #read the start of the stream into a buffer.
+    #         buf = self.r.read(2**12)
+    #         zo = zlib.decompressobj()
+    #
+    #         #start the decompression
+    #         try:
+    #             stream = zo.decompress(buf)
+    #         except zlib.error: # right magic number but not a zlib stream.
+    #             continue
+    #
+    #         while zo.unused_data == b'':
+    #             block = self.r.read(2**12)
+    #             if len(block)> 0:
+    #                 try:
+    #                     stream += zo.decompress(block)
+    #                 except zlib.error:
+    #                     pass
+    #             else:
+    #                 break # we've reached EOF
+    #
+    #         with open(self.filename + '_' + str(i) + '.decompressed', 'wb') as fh:
+    #             fh.write(stream)
+    #
+    #         i+=1
+    #
+    #     self.r.seek(curpos) #reset the file position
 
-    def read_terminated_string(self):
-        """ Read a nul-terminated string. """
-        s = ""
-        while True:
-            c = self.r.read(1)
-            if ord(c) == 0:
-                return s.decode("utf-8", 'replace')
-            s += c
 
-    def read_terminated_string_list(self):
-        """ Read a list of nul-terminated strings, terminated by a zero-length string. """
-        l = []
-        while True:
-            s = self.read_terminated_string().decode("utf-8", 'replace')
-            if s == "":
-                return l
-            l.append(s)
-
-    def read_sized_string_list(self, size):
-        """ Read a block of data with a given size, and split in null-terminated strings. """
-        block = self.r.read(size)
-        if block.endswith("\0"):
-            block = block[:-1]
-        return block.split("\0")
-
-    def peek(self):
-        curPos = self.r.tell() #save current position
-        val = self.read_int()
-        self.r.seek(curPos) #reset position
-        return val
-
-    def find(self, bytes, forward = True, offset = -4, searchFromCurrentPos = False):
-        """
-        Searches a byte object in the file.
-        forward = True : sets the position on the first occurrence of the pattern
-        returns position of the pattern of -1 if not found
-        """
-        curPos = self.r.tell()
-        if not searchFromCurrentPos:
-            self.r.seek(0)
-
-        f = Bits(self.r)
-        occ = f.find(bytes, bytealigned=True, start=int(self.r.tell()*8))
-
-        if len(occ)> 0:
-            pos = int(occ[0]/8)
-            if forward:
-                self.r.seek(pos + offset)
-            else:
-                self.r.seek(curPos)
-
-            return pos + offset
-
-        return 0 #not found
-
-    def findall(self, bytes):
-        f = Bits(self.r)
-        return map(lambda x: int(x/8), f.findall(bytes))
-
-
-    def forward_string(self, bytes):
-        return self.find(bytes, True)
-
-
-    def extract_compressed_data(self):
-        curpos = self.r.tell()
-        # self.r.seek(0)          #go the start of the file
-        # data = self.r.read()    #read all the file's data
-        # find the start of the zlib magic number 78 9C EC BD
-
-        i=0
-        occ = self.findall('0x789C')
-        for pos in occ:
-            self.r.seek(pos)
-
-            #read the start of the stream into a buffer.
-            buf = self.r.read(2**12)
-            zo = zlib.decompressobj()
-
-            #start the decompression
-            try:
-                stream = zo.decompress(buf)
-            except zlib.error: # right magic number but not a zlib stream.
-                continue
-
-            while zo.unused_data == b'':
-                block = self.r.read(2**12)
-                if len(block)> 0:
-                    try:
-                        stream += zo.decompress(block)
-                    except zlib.error:
-                        pass
-                else:
-                    break # we've reached EOF
-
-            with open(self.filename + '_' + str(i) + '.decompressed', 'wb') as fh:
-                fh.write(stream)
-
-            i+=1
-
-        self.r.seek(curpos) #reset the file position
 
 if __name__ == "__main__":
     import sys
     with FileReader(sys.argv[1]) as fr:
-        fr.extract_compressed_data()
+        pass
+        # fr.extract_compressed_data()
 
--- parse.py
+++ parse.py
@@ -13,29 +13,26 @@ __status__ = "Development"
 
 def parse(filename):
     """ Parses the save file and transforms it to xml    """
-
     root = ET.Element("root")
 
     with fr.FileReader(filename) as civ5Save:
         parseBase(civ5Save, root)
-        extract(civ5Save, root)
+        # extract(civ5Save, root)
 
     tree = ET.ElementTree(root)
     tree.write(filename + '.transformed.xml')
 
-
 def parseBase(fileReader, xml):
     """
         Parse the general game options
-        Code is definetely not optimal. We'll go thorugh a round a refactoring after mapping more information
+        Code is definitely not optimal. We'll go through a round a refactoring after mapping more information
         Refactoring 1: Remove all localization queries. This will be done on a later note.
     """
-
 
     base = ET.SubElement(xml, 'base')
     version = ET.SubElement(base , 'version')
 
-    fileReader.skip(4) #always CIV5
+    fileReader.skip_bytes(4) #always CIV5
 
     version.set('save', str(fileReader.read_int()))
     version.set('game',fileReader.read_string())
@@ -44,7 +41,7 @@ def parseBase(fileReader, xml):
     game = ET.SubElement(base, 'game')
     game.set('currentturn', str(fileReader.read_int()))
 
-    fileReader.skip(1) #TODO: I'll investigate later as to what this byte hold
+    fileReader.skip_bytes(1) #TODO: I'll investigate later as to what this byte hold
 
     civilization = ET.SubElement(base, 'civilization')
     civilization.text = fileReader.read_string()
@@ -65,46 +62,110 @@ def parseBase(fileReader, xml):
     mapscript = ET.SubElement(base, 'mapscript')
     mapscript.text = fileReader.read_string()
 
-    fileReader.skip(4) #TODO: an int
-
+    fileReader.skip_bytes(4) #TODO: an int
+    #
     dlcs = ET.SubElement(base, 'dlcs')
-    while fileReader.peek() != 0:
-        fileReader.skip(16) #TODO: some binary data
-        fileReader.skip(4) #TODO: seems to be always 1
+    while fileReader.peek_int() != 0:
+        fileReader.skip_bytes(16) #TODO: some binary data
+        fileReader.skip_bytes(4) #TODO: seems to be always 1
         dlc = ET.SubElement(dlcs, 'dlc')
         dlc.text = fileReader.read_string()
+    #
+    # #Extract block position (separated by \x40\x00\x00\x00 (@) )
+    # #I haven't decoded what each of these blocks mean but I'll extract their position for the time being.
 
-    # Fast forward to the game options position
-    fileReader.forward_string(b'GAMEOPTION')
+    bit_block_position = tuple(fileReader.findall('0x40000000'))
+    #32 blocks have been found. We'll try to map them one at a time.
 
-    gameoptions = ET.SubElement(base, 'gameoptions')
-    option = fileReader.read_string()
+    #block 1
+    fileReader.pos = bit_block_position[0] + 32 #remove the delimiter (@)
+    block1 = tuple(map(lambda x: x.read(32).intle, fileReader.read_bytes(152).cut(32)))
 
-    while option.startswith('GAMEOPTION'):
-        opt = ET.SubElement(gameoptions, 'option')
-        opt.set('enabled', str(fileReader.read_int()))
-        opt.text = option
-        option = fileReader.read_string()
+    #TODO: block2 - seems to only contain Player 1?
 
-    #Fast Forward to the game turn time to find the victory types enabled
-    fileReader.forward_string(b'TURNTIMER')
-    victorytypes = ET.SubElement(base, 'victorytypes')
-    fileReader.read_string()        #TURNTIME_XXX
-    fileReader.read_string()        #TXT_KEY_TURN_XXX
-    fileReader.read_string()        #XXX
-    fileReader.skip(25)             #TODO : Bunch of int. Don't know what they do for now
+    #block3
+    #contains the type of civilization - 03 human, 01 alive, 04 dead, 02 missing
+    fileReader.pos = bit_block_position[2] + 32
+    leader_traits = tuple(map(lambda x: x.read(32).intle, fileReader.read_bytes(256).cut(32)))
 
-    #the next 5 bytes have the 5 victory types enabled - 0/1 for disabled/enabled
-    victorytypes.set('VICTORY_TIME', str(fileReader.read_byte()))
-    victorytypes.set('VICTORY_SPACE_RACE', str(fileReader.read_byte()))
-    victorytypes.set('VICTORY_DOMINATION', str(fileReader.read_byte()))
-    victorytypes.set('VICTORY_CULTURAL', str(fileReader.read_byte()))
-    victorytypes.set('VICTORY_DIPLOMATIC', str(fileReader.read_byte()))
+    #TODO: block4
+    #TODO: block5
+    #TODO: block6
+
+    #block 7
+    # contains the list of civilizations
+    civilizations = fileReader.read_strings_from_block(bit_block_position[6] + 32, bit_block_position[7])
+
+    #block 8
+    #contains the list of leaders
+    leaders = fileReader.read_strings_from_block(bit_block_position[7] + 32, bit_block_position[8], True)
+
+    #TODO: block9-18
+
+    #block 19
+    # contains the civ states. There seems to be a whole bunch of leading 0s.
+    fileReader.forward_to_first_non_zero_byte(bit_block_position[18] + 32, bit_block_position[19])
+    civStates = fileReader.read_strings_from_block(fileReader.pos, bit_block_position[19], True)
+
+    #TODO: block 20 - there's a 16 byte long list of 01's
+    #TODO: block 21 - seems to be FFs
+    #TODO: block 22, 23 - 00s
+    #TODO: block 24 - player colors
+    #TODO: blocks 25-27
+
+    #block 28
+    #the last 5 bytes contain the enabled victory types
+    fileReader.pos = bit_block_position[28] - 5*8
+    victorytypes = (fileReader.read_byte(), fileReader.read_byte(), fileReader.read_byte(), fileReader.read_byte(), fileReader.read_byte() )
+
+    #block 29
+    # have the game options
+    fileReader.find(b'GAMEOPTION', bit_block_position[28]+32, bit_block_position[29])
+    fileReader.pos -= 32
+    gameoptions = []
+    while fileReader.pos < bit_block_position[29]:
+        s = fileReader.read_string()
+        if s == "":
+            break
+        state = fileReader.read_int()
+        gameoptions.append((s, state))
+
+    #block 30-31
+    #block 32
+    #contains the zlib compressed data
+
+    #
+    # # Fast forward to the game options position
+    # fileReader.forward_string(b'GAMEOPTION')
+    #
+    # gameoptions = ET.SubElement(base, 'gameoptions')
+    # option = fileReader.read_string()
+    #
+    # while option.startswith('GAMEOPTION'):
+    #     opt = ET.SubElement(gameoptions, 'option')
+    #     opt.set('enabled', str(fileReader.read_int()))
+    #     opt.text = option
+    #     option = fileReader.read_string()
+    #
+    # #Fast Forward to the game turn time to find the victory types enabled
+    # fileReader.forward_string(b'TURNTIMER')
+    # victorytypes = ET.SubElement(base, 'victorytypes')
+    # fileReader.read_string()        #TURNTIME_XXX
+    # fileReader.read_string()        #TXT_KEY_TURN_XXX
+    # fileReader.read_string()        #XXX
+    # fileReader.skip(25)             #TODO : Bunch of int. Don't know what they do for now
+    #
+    # #the next 5 bytes have the 5 victory types enabled - 0/1 for disabled/enabled
+    # victorytypes.set('VICTORY_TIME', str(fileReader.read_byte()))
+    # victorytypes.set('VICTORY_SPACE_RACE', str(fileReader.read_byte()))
+    # victorytypes.set('VICTORY_DOMINATION', str(fileReader.read_byte()))
+    # victorytypes.set('VICTORY_CULTURAL', str(fileReader.read_byte()))
+    # victorytypes.set('VICTORY_DIPLOMATIC', str(fileReader.read_byte()))
 
 
-def extract(fileReader, xml):
-    # fileReader.extract_compressed_data()
-    pass
+# def extract(fileReader, xml):
+#     # fileReader.extract_compressed_data()
+#     pass
 
 if __name__ == "__main__":
     import sys
